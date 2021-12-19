34ace7fd6284a3d00c828f5f0d6b369112bf51af
Shu Matsumoto <shumatsumoto0328@gmail.com>
2021-12-19 17:58:18 +0900

    Mui icons 変更

--- src/components/modules/Header.tsx
+++ src/components/modules/Header.tsx
@@ -3,14 +3,14 @@ import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
-import MenuIcon from "@mui/icons-material/Menu";
+import CloudIcon from "@mui/icons-material/Cloud";
 
 export default function ButtonAppBar() {
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
         <Toolbar>
-          <MenuIcon sx={{ mr: 2 }} />
+          <CloudIcon sx={{ mr: 2 }} />
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             Title
           </Typography>
